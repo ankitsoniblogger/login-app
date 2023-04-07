6935f0d3bd7f0ac9663082123846531e4bb5fcc8
Ankit soni <38998710+ankitsoniblogger@users.noreply.github.com>
2023-04-07 23:29:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,11 +14,7 @@ config({
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
+  cors()
 );
 
 //routes
